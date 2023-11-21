d5e10aaf74cb8ec0d0308b775a502032b0954e08
Daniela Vieira <danielav.student@hmail.com>
2023-11-21 11:07:45 -0300

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+// configuraçoes projeto
+define('HOME', "http://localhost/php_mastery/public/");
+define('PROJETO','Code Estoque');
+define('TEMPLATES',__DIR__ . '/templates');
+define('PRODUTOS', require __DIR__ . '/src/produtos.php');
+
---
+++ src/produtos.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    [
+        'codigo' => '0001',
+        'nome' => 'Produto 1',
+        'preco' => 'R$ 19,90'
+    ], [
+        'codigo' => '0002',
+        'nome' => 'Produto 2',
+        'preco' => 'R$ 0,90'
+    ],
+    [
+        'codigo' => '0003',
+        'nome' => 'Produto 3',
+        'preco' => 'R$ 89,90'
+    ],
+    [
+        'codigo' => '0004',
+        'nome' => 'Produto 4',
+        'preco' => 'R$ 119,90'
+    ]
+];
